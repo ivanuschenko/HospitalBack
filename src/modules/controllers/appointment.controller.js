3725716789430a5016d1b937585b666499c0e73d
Michael Ivanuschenko <ivanuschenkoexceedteam@gmail.com>
2022-04-08 18:26:55 +0300

    create route for update appointment

--- src/modules/controllers/appointment.controller.js
+++ src/modules/controllers/appointment.controller.js
@@ -1,15 +1,23 @@
 const Appointment = require('../../models/appointments');
 
 module.exports.getAllPatients = (req, res, next) => {
-  sickList.find().then(result => {
+  Appointment.find().then(result => {
     res.send(result);    
   });
 };
 
 module.exports.createAppointment = (req, res, next) => {
-  const list = new sickList(req.body);
-  console.log(list)
+  const list = new Appointment(req.body);  
   list.save().then(result => {  
     res.send(list);
   }).catch(err => console.log(err));
  };
+
+ module.exports.updateAppointment = (req, res, next) => {
+  const bodyId = req.query._id;  
+  Appointment.updateOne({_id: bodyId}, req.body).then(result => {
+    Appointment.find().then(result => {
+      res.send(result);
+    });
+  });
+};
