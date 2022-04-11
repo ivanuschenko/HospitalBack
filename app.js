73e7e5f1ad405d42f1d590f3e8d3e71daf49358d
Michael Ivanuschenko <ivanuschenkoexceedteam@gmail.com>
2022-04-11 17:15:08 +0300

    fix bugs

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
+require('dotenv').config();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+const url = process.env.URL
+console.log(url)
 
-const url = 'mongodb+srv://admin:admin@cluster0.pyrwd.mongodb.net/Medecine?retryWrites=true&w=majority';
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(8000, () => {
