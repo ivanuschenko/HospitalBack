ef0adcc840ab3d034834f98914174e3a9f20fc0b
Michael Ivanuschenko <ivanuschenkoexceedteam@gmail.com>
2022-04-12 15:32:57 +0300

    rewrite pull request

--- src/modules/controllers/appointment.controller.js
+++ src/modules/controllers/appointment.controller.js
@@ -9,23 +9,13 @@ module.exports.getAllList = (req, res,) => {
 };
 
 module.exports.createAppointment = (req, res) => {
-  const list = new Appointment(req.body);
-  if (!list.name || !list.password ) {
-    res.status(204).send('status 204 one of values is empty');     
+  if (!req.body.name && !req.body.password ) {
+    res.status(422).send('one of values is empty');    
   }  
+  const list = new Appointment(req.body);  
   list.save().then(result => {  
     res.send(list);
   }).catch(err => res.status(404).send(err));
  };
 
- module.exports.updateAppointment = (req, res) => {
-  const bodyId = req.query._id;
-  if (!bodyId) {
-    res.status(204).send('status 204, id value is empty');  
-  }  
-  Appointment.updateOne({_id: bodyId}, req.body).then(result => {
-    Appointment.find().then(result => {
-      res.status(200).send(result);
-    }).catch(err => res.status(404).send(err));;
-  });
-};
+ 
