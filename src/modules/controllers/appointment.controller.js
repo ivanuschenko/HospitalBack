bbed0e32ab8c9399d3184497ff96ebb09ce626cd
Michael Ivanuschenko <ivanuschenkoexceedteam@gmail.com>
2022-04-12 14:06:12 +0300

    fix bugs

--- src/modules/controllers/appointment.controller.js
+++ src/modules/controllers/appointment.controller.js
@@ -5,5 +5,5 @@ const Appointment = require('../../models/appointments');
 module.exports.getAllList = (req, res,) => {
   Appointment.find().then(result => {
     res.status(200).send(result);    
-  }).catch(err => res.status(404).send(err));;
+  }).catch(err => res.status(404).send(err));
 };
