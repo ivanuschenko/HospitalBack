62430cf94156e85df5c45a4b273dd708483cea96
Michael Ivanuschenko <ivanuschenkoexceedteam@gmail.com>
2022-04-12 10:57:09 +0300

    fix bugs in createAppointments in controller file

--- src/modules/controllers/appointment.controller.js
+++ src/modules/controllers/appointment.controller.js
@@ -1,15 +1,19 @@
+const { status } = require('express/lib/response');
 const Appointment = require('../../models/appointments');
 
-module.exports.getAllPatients = (req, res, next) => {
-  sickList.find().then(result => {
-    res.send(result);    
-  });
+
+module.exports.getAllList = (req, res,) => {
+  Appointment.find().then(result => {
+    res.status(200).send(result);    
+  }).catch(err => res.status(404).send(err));;
 };
 
-module.exports.createAppointment = (req, res, next) => {
-  const list = new sickList(req.body);
-  console.log(list)
+module.exports.createAppointment = (req, res) => {
+  const list = new Appointment(req.body);
+  if (!list.name || !list.password ) {
+    res.send('status 404 one of values is empty');     
+  }  
   list.save().then(result => {  
     res.send(list);
-  }).catch(err => console.log(err));
+  }).catch(err => res.status(404).send(err));
  };
