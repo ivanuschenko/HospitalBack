405a06b11fc3da596ebc654c719eca374310ccd5
Michael Ivanuschenko <ivanuschenkoexceedteam@gmail.com>
2022-04-12 13:31:46 +0300

    fix bug

--- src/modules/controllers/user.controller.js
+++ src/modules/controllers/user.controller.js
@@ -15,11 +15,9 @@ module.exports.singIn = (req, res) => {
   if (!req.body.name && !req.body.password ) {
     res.status(422).send('one of values is empty');    
   }
-  const body = req.body
-  const user = {
-    name: body.name,
-    password: body.password
-  }
+  const body = req.body;
+  const user = {...body};
+    
   User.findOne(user).then(result => {
     result ? res.status(200).send(result): res.status(404).send('Incorrect name or password');    
   });
