dc32c5e56662b871694acd1ee850a1be3369c60c
Michael Ivanuschenko <ivanuschenkoexceedteam@gmail.com>
2022-04-12 11:01:03 +0300

    add route for create request

--- src/modules/routes/AppoinmentRoutes.js
+++ src/modules/routes/AppoinmentRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const {
   getAllList,
+  createAppointment,
  
 } = require('../controllers/appointment.controller');
 
 router.get('/allList', getAllList);
+router.post('/createAppointment', createAppointment);
 
 module.exports = router;
