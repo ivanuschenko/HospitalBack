e850496cdcd124b8576b2a4a3c0ae0bd571ef596
Michael Ivanuschenko <ivanuschenkoexceedteam@gmail.com>
2022-04-12 13:59:43 +0300

    1

--- src/modules/controllers/appointment.controller.js
+++ src/modules/controllers/appointment.controller.js
@@ -11,9 +11,21 @@ module.exports.getAllList = (req, res,) => {
 module.exports.createAppointment = (req, res) => {
   const list = new Appointment(req.body);
   if (!list.name || !list.password ) {
-    res.send('status 404 one of values is empty');     
+    res.status(204).send('status 204 one of values is empty');     
   }  
   list.save().then(result => {  
     res.send(list);
   }).catch(err => res.status(404).send(err));
  };
+
+ module.exports.updateAppointment = (req, res) => {
+  const bodyId = req.query._id;
+  if (!bodyId) {
+    res.status(204).send('status 204, id value is empty');  
+  }  
+  Appointment.updateOne({_id: bodyId}, req.body).then(result => {
+    Appointment.find().then(result => {
+      res.status(200).send(result);
+    }).catch(err => res.status(404).send(err));;
+  });
+};
--- src/modules/routes/AppoinmentRoutes.js
+++ src/modules/routes/AppoinmentRoutes.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const {
   getAllList,
   createAppointment,
+  updateAppointment,
  
 } = require('../controllers/appointment.controller');
 
 router.get('/allList', getAllList);
 router.post('/createAppointment', createAppointment);
+router.patch('/updateAppointment', updateAppointment);
 
 module.exports = router;
