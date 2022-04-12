3a8f969b5698d443d3e02aac285287694f1372e1
Michael Ivanuschenko <ivanuschenkoexceedteam@gmail.com>
2022-04-12 13:14:56 +0300

    add validation

--- src/modules/controllers/user.controller.js
+++ src/modules/controllers/user.controller.js
@@ -1,17 +1,6 @@
 const { status } = require('express/lib/response');
 const User = require('../../models/users');
 
-module.exports.singIn = (req, res) => {
-  const body = req.body
-  const user = {
-    name: body.name,
-    password: body.password
-  }  
-  User.findOne(user).then(result => {
-    result ? res.status(200).send(result): res.status(404).send('Incorrect name or password');    
-  });
-};
-
 module.exports.createUser = (req, res) => {  
   if (!req.body.name && !req.body.password ) {
     res.status(422).send('one of values is empty');    
@@ -21,3 +10,19 @@ module.exports.createUser = (req, res) => {
     res.status(200).send(result);
   }).catch(err => res.status(404).send(err));
  };
+
+module.exports.singIn = (req, res) => {
+  if (!req.body.name && !req.body.password ) {
+    res.status(422).send('one of values is empty');    
+  }
+  const body = req.body
+  const user = {
+    name: body.name,
+    password: body.password
+  }
+  User.findOne(user).then(result => {
+    result ? res.status(200).send(result): res.status(404).send('Incorrect name or password');    
+  });
+};
+
+
