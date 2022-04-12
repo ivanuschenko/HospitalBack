681b8cc24e1919e931bedb15031baf47b34789fb
Michael Ivanuschenko <ivanuschenkoexceedteam@gmail.com>
2022-04-12 11:15:38 +0300

    fix bugs in update request

--- src/modules/controllers/appointment.controller.js
+++ src/modules/controllers/appointment.controller.js
@@ -1,23 +1,31 @@
+const { status } = require('express/lib/response');
 const Appointment = require('../../models/appointments');
 
-module.exports.getAllPatients = (req, res, next) => {
+
+module.exports.getAllList = (req, res,) => {
   Appointment.find().then(result => {
-    res.send(result);    
-  });
+    res.status(200).send(result);    
+  }).catch(err => res.status(404).send(err));;
 };
 
-module.exports.createAppointment = (req, res, next) => {
-  const list = new Appointment(req.body);  
+module.exports.createAppointment = (req, res) => {
+  const list = new Appointment(req.body);
+  if (!list.name || !list.password ) {
+    res.status(204).send('status 204 one of values is empty');     
+  }  
   list.save().then(result => {  
     res.send(list);
-  }).catch(err => console.log(err));
+  }).catch(err => res.status(404).send(err));
  };
 
- module.exports.updateAppointment = (req, res, next) => {
-  const bodyId = req.query._id;  
+ module.exports.updateAppointment = (req, res) => {
+  const bodyId = req.query._id;
+  if (!bodyId) {
+    res.status(204).send('status 204, id value is empty');  
+  }  
   Appointment.updateOne({_id: bodyId}, req.body).then(result => {
     Appointment.find().then(result => {
-      res.send(result);
-    });
+      res.status(200).send(result);
+    }).catch(err => res.status(404).send(err));;
   });
 };
