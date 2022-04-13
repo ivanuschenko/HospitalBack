a5c5a8c3ba363866ad922f61bbebf3f099a903c0
Michael Ivanuschenko <ivanuschenkoexceedteam@gmail.com>
2022-04-13 11:36:16 +0300

    create delete function

--- src/modules/routes/AppoinmentRoutes.js
+++ src/modules/routes/AppoinmentRoutes.js
@@ -4,12 +4,14 @@ const router = express.Router();
 const {
   getAllList,
   createAppointment,
-  updateAppointment
+  updateAppointment,
+  deleteAppointment
  
 } = require('../controllers/appointment.controller');
 
 router.get('/allList', getAllList);
 router.post('/createAppointment', createAppointment);
 router.patch('/updateAppointment', updateAppointment);
+router.delete('/deleteAppointment', deleteAppointment);
 
 module.exports = router;
