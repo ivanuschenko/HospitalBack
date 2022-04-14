71da097c568731933476a0df4f00139b894b1dc6
Michael Ivanuschenko <ivanuschenkoexceedteam@gmail.com>
2022-04-14 14:06:43 +0300

    create folders  middleware and exceptions, create function signIn and signOut

--- src/modules/controllers/user.controller.js
+++ src/modules/controllers/user.controller.js
@@ -1,29 +1,38 @@
 const { status } = require('express/lib/response');
 const User = require('../../models/users');
 const userService = require('../service/user-service');
+const ApiError = require('../../exceptions/api-error');
 
-module.exports.registration = async (req, res) => { 
-  try {
-    if (!req.body.name && !req.body.password ) {
-      throw new Error(res.status(422).send('one of values is empty'));     
-    }  
+module.exports.registration = async (req, res, next) => { 
+  try {     
     const {name, password} = req.body;
     const userData = await userService.registration(name, password);
     res.cookie('refreshToken', userData.refreshToken, {maxAge: 30*24*60*60*1000, httpOnly: true})
     return res.json(userData); 
   } catch (e) {
-    res.status(401).send('Incorect login or password');    
+      next(e)        
   }
 }
  
-module.exports.signIn = (req, res) => {
-  if (!req.body.name && !req.body.password ) {
-    res.status(422).send('one of values is empty');    
-  }  
-  const user = {...req.body};
-    
-  User.findOne(user).then(result => {
-    result ? res.status(200).send(result): res.status(404).send('Incorrect name or password');    
-  });
-};
- 
+module.exports.signIn = async (req, res, next) => {  
+  try {
+    const {name, password} = req.body;
+    const userData = await userService.signIn(name, password);
+    res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+    return res.json(userData);
+  } catch (e) {
+      next(e);
+    }
+}
+module.exports.signOut = async (req, res, next) => {  
+  try {
+      const {refreshToken} = req.cookies;
+      const token = await userService.signOut(refreshToken);
+      res.clearCookie('refreshToken');
+      return res.json(token);
+  } catch (e) {
+      next(e);
+  }
+  
+}
+
